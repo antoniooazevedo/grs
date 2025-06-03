ef3c7d75de4325c6a775397a9cb18b6ee24fa85b
Antonio Azevedo <antazevedo2003@gmail.com>
2025-06-03 22:53:51 +0100

    changed demo script's comments

--- proj/demo.sh
+++ proj/demo.sh
@@ -23,7 +23,7 @@ done
 echo "Initializing keyspace with RF=3, and inserting a bunch of data"
 ./scripts/init_keyspace_and_tables.sh
 
-echo "Sleeping for 60 seconds in order to generate hinted handoffs"
+echo "Sleeping for 90 seconds in order to generate hinted handoffs"
 for ((i=0; i<90; i++)); do
     echo -ne "Slept for: $i\r"
     sleep 1
